3c9edb41797716cf7d8d2721f36f7327e144d7c9
William <willspri@gmail.com>
2024-03-06 11:33:23 +1300

    upated

--- vowelCount.java
+++ vowelCount.java
@@ -8,15 +8,15 @@ public class vowelCount {
 		String input = scanner.nextLine();
 
 		int vowelCount = countVowels(input);
-		
+
 		System.out.println("Vowel Count: " + vowelCount);
 	}
 
 	public static int countVowels(String str) {
 		int count = 0;
 		//Convert to lowercase to handle both cases
-
 		str = str.toLowerCase();
+		//check each index of string
 		for (int i = 0; i <str.length(); i++) {
 			char ch = str.charAt(i);
 			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
